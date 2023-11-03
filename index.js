dc97a937b79f9c7a80ffab0a05e6ab77d74608e1
OrianaPG <68253167+OrianaPG@users.noreply.github.com>
2023-11-03 19:05:38 -0300

    Eliminé código viejo

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-/*
-let containerlenguajes = document.getElementById("containerlenguajes");
-
-let respuesta = fetch("https://www.preguntapi.dev/api/categories",{
-    method:"GET",
-    mode:"no-cors",
-
-})
-.then(respuesta => respuesta.json())
-.then(data => {
-    let lenguajes = data.map(lenguajes => 
-        `<button class="btn btn-primary">${lenguajes.name}</button>`    
-    );
-    containerlenguajes.innerHTML = lenguajes.join();
-})
-.catch(error => console.log("ERROR FATAL", error));
-*/
-
 let containerlenguajes = document.getElementById("containerlenguajes");
 
 const url = 'https://corsproxy.io/?' + encodeURIComponent('https://www.preguntapi.dev/api/categories');
 
-fetch(url, {
-    method: "GET"
-    //mode: "no-cors"  // Cambiado a "cors" para permitir solicitudes a otro dominio.
-})
+fetch(url)
     .then(respuesta => respuesta.json())
     .then(data => {
         let lenguajes = data.categories.map(lenguajes =>
             `<button class="btn btn-primary">${lenguajes.name}</button>`
         );
-        containerlenguajes.innerHTML = lenguajes.join(" ");  // Utiliza join("") para unir sin separadores.
+        containerlenguajes.innerHTML = lenguajes.join(" ");
     })
     .catch(error => console.log("ERROR FATAL", error));
