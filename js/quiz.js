f388032bcd75a02213347017b03428bb01b5ae07
OrianaPG <68253167+OrianaPG@users.noreply.github.com>
2023-11-10 17:46:03 -0300

    Agregué esqueleto para validar respuesta

--- js/quiz.js
+++ js/quiz.js
@@ -1,12 +1,35 @@
 category = localStorage.getItem("lenguaje");
-//category = "javascript";
 nivel = localStorage.getItem("dificultad");
 cantidadPreguntas = localStorage.getItem("cantidadPreguntas")
-let preguntas;
 
-ciclo = 0;
+let preguntas;
+let puntuacionActual = 0;
+let ciclo = 0;
+
 let preguntaRespuestas = document.getElementById("preguntaRespuestas");
+
 const url = 'https://corsproxy.io/?' + encodeURIComponent(`https://www.preguntapi.dev/api/categories/${category}?level=${nivel}&limit=${cantidadPreguntas}`);
+
+function agregarModal(puntuacionActual, cantidadPreguntas) {
+    let botonSiguiente = document.getElementById("siguientePregunta");
+    botonSiguiente.setAttribute("data-bs-toggle", "modal");
+    botonSiguiente.setAttribute("data-bs-target", "#staticBackdrop");
+
+    let modal = document.getElementById("modal")
+    modal.innerHTML =
+        `Tu puntuación final fue de <span class="bold">${puntuacionActual}/${cantidadPreguntas}</span>.
+            <br>
+            ¡Muchas gracias por jugar!
+            <br>
+            Si querés volver a intentarlo con las mismas preguntas, dale a "Repetir quiz". Sino, dale a "Salir".`
+}
+
+function validarRespuesta(pregunta, respuestaSeleccionada) {
+    if (pregunta.correct_answer === respuestaSeleccionada) {
+        return true;
+    }
+    return false;
+}
 
 async function levantarPreguntas(url) {
     const respuesta = await fetch(url);
@@ -15,54 +38,47 @@ async function levantarPreguntas(url) {
 
 async function obtenerPreguntas(ciclo) {
     preguntas = await levantarPreguntas(url);
-    let puntuacionActual = 0;
 
-    //for (let indice = 0; indice < preguntas.length; indice++) {
-    // Cargamos la pregunta y la puntuación
-
-
-    // Cargamos la respuesta (https://stackoverflow.com/questions/14379274/how-to-iterate-over-a-javascript-object)
-    // for (let [key, value] of Object.entries(pregunta.answers)) {
-    //     preguntaRespuestas.innerHTML += `<button class="btn btn-warning">${value}</button>`;
-    //     }
-
-    // Chequeamos que la respuesta seleccionada sea la correcta
-
-    //}
     console.log(preguntas);
 }
 
 function obtenerUnaPregunta() {
-    let puntuacionActual = 0;
-    //obtenerPreguntas(ciclo);
     const pregunta = preguntas[ciclo];
     preguntaRespuestas.innerHTML = `<h1>${pregunta.category.toUpperCase()}</h1>
             <p>Puntuación: ${puntuacionActual} de ${cantidadPreguntas}</p>
             <p>Pregunta: ${ciclo + 1} de ${cantidadPreguntas}</p>
             <p class="bold">${pregunta.question}</p>`;
 
-
-    if (ciclo + 1 == cantidadPreguntas) {
-        let botonSiguiente = document.getElementById("siguientePregunta");
-        botonSiguiente.setAttribute("data-bs-toggle", "modal");
-        botonSiguiente.setAttribute("data-bs-target", "#staticBackdrop");
-
-        let modal = document.getElementById("modal")
-        modal.innerHTML =
-            `Tu puntuación final fue de <span class="bold">${puntuacionActual}/${cantidadPreguntas}</span>.
-            <br>
-            ¡Muchas gracias por jugar!
-            <br>
-            Si querés volver a intentarlo con las mismas preguntas, dale a "Repetir quiz". Sino, dale a "Salir".`
+    // Cargamos la respuesta
+    for (let [key, value] of Object.entries(pregunta.answers)) {
+        preguntaRespuestas.innerHTML += `<button class="btn btn-warning" id="${key}">${value}</button>`;
     }
 
-    //console.log(preguntas[ciclo].question);
+    //! De alguna manera obtener la respuesta correcta (se me ocurre que con un event listener que guarde el id de la respuesta seleccionada)
+
+    if (validarRespuesta(pregunta, respuestaSeleccionada)) {
+        puntuacionActual++;
+        //! Cambiarle el color a la respuesta correcta (que es la seleccionada), pasar de btn-warning a btn-success
+        //! A los otros botones agregarles el atributo disabled para que no puedan ser clickeados
+        //! Sacarle el atributo disabled al botón de Siguiente
+    }
+    else {
+        //! Cambiarle el color a la respuesta correcta, pasar de btn-warning a btn-success
+        //! Cambiarle el color a la respuesta incorrecta (la seleccionada), pasar de btn-warning a btn-danger
+        //! A los otros botones agregarles el atributo disabled para que no puedan ser clickeados
+        //! Sacarle el atributo disabled al botón de Siguiente
+    }
+
+    if (ciclo + 1 == cantidadPreguntas) {
+        agregarModal(puntuacionActual, cantidadPreguntas);
+    }
+
     console.log(pregunta.question);
     ciclo++;
 }
 
 function repetirQuiz() {
-    // Que te devuelva a la primera pregunta
+    //! Que te devuelva a la primera pregunta
 }
 
 function volverAlInicio() {
